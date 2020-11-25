0477fa52c7b4bddbea6719b8896d0fce184ba006
Benjamin Suter <BenjaminSuter@student-net-vpn-1136.ethz.ch>
2020-11-25 19:57:20 +0100

    NGSF version

---
+++ NGSFapps/prepLIGHTCONES.py
@@ -0,0 +1,113 @@
+import os
+import sys
+import logging
+import numpy as np
+import healpy as hp
+
+from DeepSphere.utils import extend_indices
+from estats.catalog import catalog
+from utils import _get_config
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+
+def _rotate_maps(map, ctx, downsampling=False):
+    """
+    Will rotate maps into position of MASK/TRIMMED-MASK_cut=0 and adjust for equal size
+    :param map: A dictionary of Healpix convergence maps
+    :param ctx: Context instance
+    :returns dataset: tf.data.dataset containing the mask of the rotated maps
+             rotated_indices_ext: np.array containing the healpy indices of interest in nested order.
+    """
+    indices = np.arange(len(map))[map > hp.UNSEEN]
+    delta, alpha = hp.pix2ang(ctx["NSIDE"], indices)
+
+    mock_cat = catalog(alphas=alpha,
+                       deltas=delta,
+                       degree=False,
+                       colat=True)
+    alpha, delta = mock_cat._rotate_coordinates(
+        alpha_rot=2 * np.pi -
+                  ctx["alpha_rotations"][ctx["index_counter"]],
+        delta_rot=2 * np.pi -
+                  ctx["dec_rotations"][ctx["index_counter"]],
+        mirror=ctx["mirror"][ctx["index_counter"]])
+    pix = mock_cat._pixelize(alpha, delta)
+    rotated_indices_ext = extend_indices(
+        indices=pix,
+        nside_in=ctx["NSIDE"],
+        nside_out=ctx["NSIDE_OUT"],
+        nest=False)
+    rotated_map = np.full_like(map, hp.UNSEEN)
+    zero_padding = np.zeros_like(map)
+    rotated_map[rotated_indices_ext] = zero_padding[
+        rotated_indices_ext]
+    rotated_map[pix] = map[indices]
+
+    if downsampling:
+        rotated_map = hp.ud_grade(rotated_map, downsampling, order_out="NESTED")
+    else:
+        rotated_map = hp.reorder(rotated_map, r2n=True)
+
+    return rotated_map
+
+
+def LSF_map_rotator(job_index,
+                    path="/cluster/work/refregier/besuter/data/LIGHTCONES",
+                    mask_path = "/cluster/work/refregier/besuter/data/mask",
+                    target="NGSF_LIGHTCONES",
+                    downsampling=512,
+                    cosmo_conf="z=0.0_0",
+                    mask_count=8,
+                    NSIDE=1024,
+                    NSIDE_OUT=32,
+                    SCRATCH=True):
+    """
+    This function is meant to be used in the context of lsf job array.
+    """
+    ctx = {
+        "NSIDE": NSIDE,
+        "NSIDE_OUT": NSIDE_OUT,
+        "alpha_rotations": [0., 1.578, 3.142, 4.712, 0., 1.578, 3.142, 4.712],
+        "dec_rotations": [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+        "mirror": [False, False, False, False, True, True, True, True],
+        "scales": [2.6]
+    }
+    # Change the cosmo_files!  --> We want to use the LIGHTCONES.
+    cosmo_file = _get_config("/cluster/work/refregier/besuter/LIGHTCONE.ini")[cosmo_conf][str(
+        job_index)]
+    map_path = os.path.join(path, cosmo_file)
+    if SCRATCH:
+        target_path = os.path.join(os.path.expandvars("$SCRATCH"), target)
+    else:
+        target_path = target
+    os.makedirs(target_path, exist_ok=True)
+    try:
+        k_map_unsmoothed = hp.read_map(map_path)
+    except IOError as e:
+        logger.critical(e)
+        sys.exit(1)
+
+    for cut in range(mask_count):
+        ctx["index_counter"] = cut
+        logger.info(
+            f"Applying mask cut={cut} and rotating map back.")
+        for scale in ctx["scales"]:
+            k_map = hp.sphtfunc.smoothing(
+                k_map_unsmoothed, fwhm=np.radians(float(scale) / 60.))
+            mask = hp.read_map(os.path.join(mask_path, f"TRIMMED-MASK_cut={cut}_tomo=1.fits"))
+            mask = np.logical_not(mask)
+            k_map[mask] = hp.pixelfunc.UNSEEN
+            k_map = _rotate_maps(k_map, ctx, downsampling=downsampling)
+
+            outfile_name = cosmo_file[:-5] + f"_cut={cut}_scale={scale}.npy"
+            final_target_path = os.path.join(target_path, outfile_name)
+            np.save(final_target_path, k_map)
---
+++ NGSFapps/tfrecord_writer.py
@@ -0,0 +1,86 @@
+import os
+import re
+import sys
+import uuid
+import logging
+import numpy as np
+import healpy as hp
+
+from DeepSphere import data
+from DeepSphere.utils import extend_indices
+#from estats.catalog import catalog
+from utils import _write_tfr
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+
+
+def _label_finder(str):
+    Om_label = re.search(r"(?<=Om=).+(?=_eta)", str).group(0)
+    s8_label = re.search(r"(?<=s8=).+(?=_tomo)", str).group(0)
+    return (float(Om_label), float(s8_label))
+
+
+def _noise_info(str):
+    tomo = re.search(r"(?<=tomo=).+(?=_z)", str).group(0)
+    scale = re.search(r"(?<=scale=).+(?=.n)", str).group(0)
+    return float(tomo)
+
+
+def LSF_tfrecord_writer(job_index,
+                        path="NGSF_LIGHTCONES",
+                        target="NGSFrecords",
+                        file_count=32,
+                        SCRATCH=True):
+    """
+    ToDo: Find better way of generating the cosmology labels
+    This function is meant to be used in the context of lsf job array.
+    :param job_index: int, corresponds to the job index of the job array has to be [1-file_count].
+    """
+    # Job index starts at 1, array index starts at 0
+    array_idx = int(job_index) - 1
+    if SCRATCH:
+        map_path = os.path.join(os.path.expandvars("$SCRATCH"), path)
+        target_path = os.path.join(os.path.expandvars("$SCRATCH"), target)
+    else:
+        map_path = path
+        target_path = target
+    f_names = [
+        os.path.join(map_path, file) for file in os.listdir(map_path)
+        if not file.startswith(".")
+    ]
+    batch_size = int(len(f_names) / file_count)
+    start = int(array_idx * batch_size)
+    if array_idx + 1 == file_count:
+        name_batch = f_names[start:]
+        logger.info(f"Serializing maps {start} to end")
+    else:
+        end = int(start + batch_size)
+        name_batch = f_names[start:end]
+        logger.info(f"Serializing maps {start} to {end}")
+    serialized_example_dump = []
+    for file in name_batch:
+        try:
+            k_map = np.load(file)
+        except IOError as e:
+            logger.critical(e)
+            continue
+        labels = _label_finder(file)
+        noise_info = _noise_info(file)
+        cosmo_labels = np.array([
+            [labels[0], labels[1]], [noise_info[0], noise_info[1]]
+        ])
+        serialized_example_dump.append(
+            data.serialize_labeled_example(k_map, cosmo_labels))
+    logger.info("Dumping maps")
+    tfrecord_name = f"kappa_map_cosmo={start}-{end-1}_shapes={np.shape(k_map)},{np.shape(cosmo_labels)}_total_{uuid.uuid4().hex}.tfrecord"
+    target_path = os.path.join(target_path, tfrecord_name)
+    _write_tfr(serialized_example_dump, target_path)
+    serialized_example_dump.clear()
