be0b51b45749029e2a0da08f5dd909f699541139
Benjamin Suter <BenjaminSuter@student-net-vpn-1136.ethz.ch>
2020-11-25 19:06:24 +0100

    Typo

--- ReadLIGHTCONE.py
+++ ReadLIGHTCONE.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     config.add_section("z=0.0_0")
     lightcone_files = [
         file for file in os.listdir(path)
-        if file.endswith("z=0.0_0.fits.npy") and file.startswith("LIGHTCONE")
+        if file.endswith("z=0.0_0.fits") and file.startswith("LIGHTCONE")
     ]
     for idx, file in enumerate(lightcone_files):
         config.set("z=0.0_0", str(idx), file)
