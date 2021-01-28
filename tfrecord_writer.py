fc492498cd6f486e1459054485ecfa8f03f7687e
Benjamin Suter <BenjaminSuter@student-net-vpn-2324.ethz.ch>
2021-01-28 21:38:48 +0100

    First version

--- tfrecord_writer.py
+++ tfrecord_writer.py
@@ -2,5 +2,7 @@ from utils import tfrecord_writer
 
 if __name__ == "__main__":
     print("Starting TFRecord_writer")
-    tfrecord_writer("/scratch/snx3000/bsuter/kappa_maps", target="TFRecordsMasked", downsampling=256)
+    tfrecord_writer("/scratch/snx3000/bsuter/kappa_maps",
+                    target="TFRecordsMasked",
+                    downsampling=256)
     print("Done")
