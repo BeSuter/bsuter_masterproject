c19f5eee87eca8b731f8e41497b45c05822924fa
Benjamin Suter <ben.suter@bluewin.ch>
2021-03-22 16:14:11 +0100

    Testing memory on daint

---
+++ TestScripts/memory_test.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import psutil
+
+
+if __name__ == "__main__":
+    dataset = tf.Dataset.range(int(1e7))
+    iterator = dataset.shuffle(int(1e7)).batch(int(1e6))
+
+    print("")
+    print("##############################################")
+    print("############# STARTING ITERATION #############")
+
+    for _ in iterator:
+        used_mem = psutil.virtual_memory().used
+        print("used memory: {} Mb".format(used_mem / 1024 / 1024))
